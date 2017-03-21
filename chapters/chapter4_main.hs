843cf4df5fed1ae1700c122b7e52ff29cdfc2e01
Mariana Bravo <marivb@gmail.com>
2017-03-20 23:23:54 -0300

    chapter 4

---
+++ chapters/chapter4_main.hs
@@ -0,0 +1,32 @@
+module Main where
+
+import Draw
+import Shape
+import SOE
+
+sh1, sh2, sh3, sh4 :: Shape
+
+sh1 = Rectangle 3 2
+sh2 = Ellipse 1 1.5
+sh3 = RtTriangle 3 2
+sh4 = Polygon [(-2.5, 2.5), (-1.5, 2.0), (-1.1, 0.2),
+               (-1.7, -1.0), (-3.0, 0)]
+
+type ColoredShapes = [(Color, Shape)]
+shs :: ColoredShapes
+shs = [(Red, sh1),
+       (Blue, sh2),
+       (Yellow, sh3),
+       (Magenta, sh4)]
+
+drawShapes :: Window -> ColoredShapes -> IO ()
+drawShapes w [] = return ()
+drawShapes w ((c,s) : cs) =
+  do drawInWindow w (withColor c (shapeToGraphic s))
+     drawShapes w cs
+
+main = runGraphics (
+  do w <- openWindow "Drawing Shapes" (xWin, yWin)
+     drawShapes w shs
+     spaceClose w
+  )
---
+++ modules/Draw.hs
@@ -0,0 +1,46 @@
+module Draw (inchToPixel, pixelToInch, intToFloat,
+             xWin, yWin, trans, shapeToGraphic, spaceClose
+  ) where
+
+import Shape
+import SOE
+
+inchToPixel :: Float -> Int
+inchToPixel x = round (100 * x)
+
+pixelToInch :: Int -> Float
+pixelToInch n = intToFloat n/100
+
+intToFloat :: Int -> Float
+intToFloat n = fromInteger (toInteger n)
+
+xWin, yWin :: Int
+xWin = 600
+yWin = 500
+
+trans :: Vertex -> Point
+trans (x, y) = (midWinX + inchToPixel x, midWinY + inchToPixel y)
+
+midWinX, midWinY :: Int
+midWinX = div xWin 2
+midWinY = div yWin 2
+
+transList :: [Vertex] -> [Point]
+transList [] = []
+transList (p:ps) = trans p : transList ps
+
+shapeToGraphic :: Shape -> Graphic
+shapeToGraphic (Rectangle s1 s2) =
+  let x = s1 / 2
+      y = s2 / 2
+  in polygon (transList [(-x, -y), (-x, y),
+                         (x, y), (x, -y)])
+
+shapeToGraphic (Ellipse r1 r2) = ellipse (trans (-r1, -r2)) (trans (r1, r2))
+shapeToGraphic (RtTriangle s1 s2) = polygon (transList [(0, 0), (s1, 0), (0, s2)])
+shapeToGraphic (Polygon vts) = polygon (transList vts)
+
+spaceClose :: Window -> IO ()
+spaceClose w = do k <- getKey w
+                  if k == ' ' then closeWindow w
+                              else spaceClose w
