c998e52c27a218b463c9a0db835297e7e930ef1b
Mariana Bravo <marivb@gmail.com>
2017-03-20 22:53:15 -0300

    chapter 3

---
+++ chap3/exercise3.hs
@@ -0,0 +1,3 @@
+module Chapter3Exercises where
+
+-- 3.2 Draw snowflake fractal
--- chap3/main.hs
+++ chap3/main.hs
@@ -2,7 +2,7 @@ module Main where
 
 import SOE
 
-main = runGraphics (
+main1 = runGraphics (
     do w <- openWindow "Meu Primeiro Programa Grafico" (300, 300)
        drawInWindow w (text (100, 200) "Ola Mundo Grafico")
        spaceClose w
@@ -13,4 +13,34 @@ spaceClose w = do k <- getKey w
                   if k == ' ' then closeWindow w
                               else spaceClose w
 
+pic1 = withColor Red (ellipse (150, 150) (300, 200))
+pic2 = withColor Blue (polyline [(100, 50), (200, 50), (200, 250), (100, 250), (100, 50)])
 
+main2 = runGraphics (
+    do w <- openWindow "figuras" (300, 300)
+       drawInWindow w pic1
+       drawInWindow w pic2
+       spaceClose w
+    )
+
+fillTriangle :: Window -> Int -> Int -> Int -> IO ()
+fillTriangle w x y size = drawInWindow w (withColor Blue
+                                         (polygon [(x, y), (x + size, y), (x, y-size)]))
+
+minSize :: Int
+minSize = 8
+
+sierpinskiTriangle :: Window -> Int -> Int -> Int -> IO ()
+sierpinskiTriangle w x y size =
+  if size <= minSize
+     then fillTriangle w x y size
+     else let size2 = size `div` 2
+          in do sierpinskiTriangle w x y size2
+                sierpinskiTriangle w x (y-size2) size2
+                sierpinskiTriangle w (x+size2) y size2
+
+main = runGraphics (
+    do w <- openWindow "Sierpinski Triangle" (400, 400)
+         sierpinskiTriangle w 50 300 256
+       spaceClose w
+    )
