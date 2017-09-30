f1343eb4a764d4287d8f72077649ebfa4f62d633
Mariana Bravo <marivb@gmail.com>
2017-09-30 19:02:37 -0300

    adds doctest examples

--- README.md
+++ README.md
@@ -5,7 +5,8 @@
 ```
 apt install haskell-platform
 cabal update
-cabal install HGL
+cabal install HGL doctest
+echo "export PATH="$HOME/.cabal/bin:$PATH" >> ~/.bashrc
 ```
 
 To see if it worked:
@@ -13,4 +14,5 @@ To see if it worked:
 ```
 ./compile chapters/chapter3_main.hs
 ./chapters/chapter3_main
+doctest chapters/chapter7.hs
 ```
--- chapters/chapter7.hs
+++ chapters/chapter7.hs
@@ -1,38 +1,71 @@
-module Main where
+module Chapter7 where
 
 -- Trees
+-- $setup
+-- >>> let tree = Branch (Branch (Leaf 2) (Leaf 1)) (Leaf 10)
+
 data Tree a = Leaf a
             | Branch (Tree a) (Tree a)
+            deriving Show
 
+-- | Maps values in a tree
+--
+-- Examples:
+--
+-- >>> mapTree (* 2) tree
+-- Branch (Branch (Leaf 4) (Leaf 2)) (Leaf 20)
 mapTree :: (a -> b) -> Tree a -> Tree b
 mapTree f (Leaf x) = Leaf (f x)
 mapTree f (Branch t1 t2) = Branch (mapTree f t1) (mapTree f t2)
 
+-- | Flattens leaf values into list
+--
+-- Examples:
+--
+-- >>> fringe tree
+-- [2,1,10]
 fringe :: Tree a -> [a]
 fringe (Leaf x) = [x]
 fringe (Branch t1 t2) = fringe t1 ++ fringe t2
 
+-- | Calculates number of leafs in a tree
+--
+-- Examples:
+--
+-- >>> treeSize tree
+-- 3
 treeSize :: Tree a -> Integer
-treeSize (Leaf f) = 0
+treeSize (Leaf f) = 1
 treeSize (Branch t1 t2) = treeSize t1 + treeSize t2
 
+-- | Calculates tree height
+--
+-- Examples:
+--
+-- >>> treeHeight tree
+-- 2
 treeHeight :: Tree a -> Integer
 treeHeight (Leaf f) = 0
 treeHeight (Branch t1 t2) = 1 + max (treeHeight t1) (treeHeight t2)
 
 -- Expressions
+
 data Expr = C Float
           | Expr :+ Expr
           | Expr :- Expr
           | Expr :* Expr
           | Expr :/ Expr
 
+-- | Evaluates an arithmetic expression
+--
+-- Examples:
+--
+-- >>> let e = (C 10 :+ (C 8 :/ C 2)) :* (C 7 :- C 4)
+-- >>> evaluate e
+-- 42.0
 evaluate :: Expr -> Float
 evaluate (C x) = x
 evaluate (e1 :+ e2) = evaluate e1 + evaluate e2
 evaluate (e1 :- e2) = evaluate e1 - evaluate e2
 evaluate (e1 :* e2) = evaluate e1 * evaluate e2
 evaluate (e1 :/ e2) = evaluate e1 / evaluate e2
-
-e = (C 10 :+ (C 8 :/ C 2)) :* (C 7 :- C 4)
-main = do putStrLn (show (evaluate e))
